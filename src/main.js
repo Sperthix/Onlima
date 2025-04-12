f9452935e2802d874d51d57b94533f49cb434a53
Matúš Búci <matusbuci@gmail.com>
2025-04-13 00:52:12 +0200

    feat: added base content

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-import './style.css'
+import './style.css';
+import Swiper from 'swiper';
+import 'swiper/css';
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
