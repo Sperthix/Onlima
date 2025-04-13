2a323e6fe42c4292865c6ff845d665fcf070f142
Matúš Búci <matusbuci@gmail.com>
2025-04-13 11:27:29 +0200

    feat: added faq accordion logic

--- src/main.js
+++ src/main.js
@@ -19,3 +19,26 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   }
 });
+
+// FAQ 
+document.querySelectorAll('.faq-toggle').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const contentEl = document.getElementById(btn.dataset.target)
+    const icon = btn.querySelector('img')
+
+    document.querySelectorAll('.faq-content').forEach(el => {
+      if (el !== contentEl) {
+        el.style.maxHeight = 0
+        el.previousElementSibling.querySelector('img').classList.remove('rotate-180')
+      }
+    })
+
+    if (contentEl.style.maxHeight && contentEl.style.maxHeight !== '0px') {
+      contentEl.style.maxHeight = 0
+      icon.classList.remove('rotate-180')
+    } else {
+      contentEl.style.maxHeight = contentEl.scrollHeight + 'px'
+      icon.classList.add('rotate-180')
+    }
+  })
+})
